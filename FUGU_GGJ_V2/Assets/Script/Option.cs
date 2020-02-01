648d0cc68fc2a043f2925067cd54f361ab7989c0
b1017159 <b1017159@fun.ac.jp>
2020-02-01 14:26:31 +0900

    時止め導入

---
+++ FUGU_GGJ_V2/Assets/Script/Option.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Option : MonoBehaviour
+{
+        public GameObject Slider;
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+        }
+        public void OnClick()
+        {//押されたときの処理
+                if(Time.timeScale != 0f ) {//時止め
+                        Slider.gameObject.SetActive(true);
+                        Time.timeScale = 0f;
+                }else{//解除
+                        Slider.gameObject.SetActive(false);
+                        Time.timeScale = 1f;
+                }
+        }
+}
