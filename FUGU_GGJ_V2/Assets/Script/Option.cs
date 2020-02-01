1173f97ad0bb1e2b6f93dea3eacc6632aa7088c0
b1017159 <b1017159@fun.ac.jp>
2020-02-01 17:07:32 +0900

    音声ファイルが入ったフォルダの場所を変更

--- FUGU_GGJ_V2/Assets/Script/Option.cs
+++ FUGU_GGJ_V2/Assets/Script/Option.cs
@@ -1,26 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[DefaultExecutionOrder(-100)]
 public class Option : MonoBehaviour
 {
         public GameObject Slider;
-        void Start()
-        {
-
-        }
-
-        void Update()
-        {
-        }
         public void OnClick()
         {//押されたときの処理
                 if(Time.timeScale != 0f ) {//時止め
                         Slider.gameObject.SetActive(true);
                         Time.timeScale = 0f;
+                        return;
                 }else{                      //解除
                         Slider.gameObject.SetActive(false);
                         Time.timeScale = 1f;
+                        return;
                 }
                 return;
         }
