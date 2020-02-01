15335cc62bd990fec0e589c497fb319414a5dddc
b1017159 <b1017159@fun.ac.jp>
2020-02-01 18:42:15 +0900

    バグ直し用のフラグをOptionに建てた

--- FUGU_GGJ_V2/Assets/Script/Option.cs
+++ FUGU_GGJ_V2/Assets/Script/Option.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[DefaultExecutionOrder(-100)]
 public class Option : MonoBehaviour
 {
         public GameObject Slider;
+        public static bool swich=false;//ボタンをクリックしたとき用フラグ
         void Start()
         {
 
@@ -15,10 +16,11 @@ public class Option : MonoBehaviour
         }
         public void OnClick()
         {//押されたときの処理
+                swich=true;
                 if(Time.timeScale != 0f ) {//時止め
                         Slider.gameObject.SetActive(true);
                         Time.timeScale = 0f;
-                }else{                      //解除
+                }else{//解除
                         Slider.gameObject.SetActive(false);
                         Time.timeScale = 1f;
                 }
