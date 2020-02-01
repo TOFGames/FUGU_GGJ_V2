26b59d8cb326232078f48c63b898406cb1568570
TOFGAMES <tofgames555@gmail.com>
2020-02-01 17:58:55 +0900

    更生後のサラリーマンの加速をなくした

--- FUGU_GGJ_V2/Assets/proto_abo/Scripts/NormalMan.cs
+++ FUGU_GGJ_V2/Assets/proto_abo/Scripts/NormalMan.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 //using UnityEngine.UI;
 
 public class NormalMan : MonoBehaviour {
+    private FlowController flowController;
+
     private float speed = 0.05f;
     private float angle = 0;
 
@@ -11,6 +13,7 @@ public class NormalMan : MonoBehaviour {
 
     //=============================================================
     private void Init () {
+        flowController = GetComponent<FlowController>();
     }
 
     private void Awake () {
@@ -19,7 +22,8 @@ public class NormalMan : MonoBehaviour {
 
     private void Start () {
         if(IsRepair) {
-            StartCoroutine(Walk());
+            Destroy(flowController);
+            StartCoroutine(Walk(3));
         }
     }
 
@@ -27,11 +31,19 @@ public class NormalMan : MonoBehaviour {
 
     }
 
-    private IEnumerator Walk () {
-        angle = Random.Range(90+15,270-15);
+    private IEnumerator Walk (float life) {
+        angle = Random.Range(90 + 15,270 - 15);
+
+        float time = 0;
         while(true) {
             transform.position += new Vector3(Mathf.Cos(Mathf.Deg2Rad * (angle - 90)),0,-Mathf.Sin(Mathf.Deg2Rad * (angle - 90))) * speed;
             transform.eulerAngles = new Vector3(0,angle,0);
+
+            time += Time.deltaTime;
+            if(time >= life) {
+                Destroy(this.gameObject);
+                break;
+            }
             yield return null;
         }
     }
