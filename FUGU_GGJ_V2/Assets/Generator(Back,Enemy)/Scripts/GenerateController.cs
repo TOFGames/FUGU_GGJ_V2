30232d9bc9afa8c3c7471903b6555a48cb7dad6f
jyackfrost123 <jyackfrost123@gmail.com>
2020-02-02 00:45:20 +0900

    someminimimum changes of generate

--- FUGU_GGJ_V2/Assets/Generator(Back,Enemy)/Scripts/GenerateController.cs
+++ FUGU_GGJ_V2/Assets/Generator(Back,Enemy)/Scripts/GenerateController.cs
@@ -51,7 +51,39 @@ public class GenerateController : MonoBehaviour
         //if(gameStart){}
 
         //for manyPrefabs to random
-         int i = Random.Range(0, prefabs.Length);
+
+        /*
+
+        if(score < 30){//Enden World
+
+        }else if(30 <= socre && score < 50) {//MAD MAX
+        
+        }else if(50 <= socre && score < 70){//Danger
+
+        }else if(70 <= socre && score < 85){//Safety
+
+        }else{//Peaceful
+
+        }
+
+        */
+
+        float rand = Random.Range(0.0f, 1.0f);
+        float border = 0.5f;
+        //border = score / 100; //0.3とかMaxわからないと何ともか?
+
+
+         //int i = Random.Range(0, prefabs.Length);
+
+         int i;
+
+         if( rand <= border ){
+             i = 0;//Hyahaaaa
+         }else{
+             i = 1;//NomalMan
+         }
+
+
          GameObject obj=Instantiate(prefabs[i], new Vector3(Random.Range(leftX, rightX), this.transform.position.y, frontZ), Quaternion.identity) as GameObject;
          obj.transform.eulerAngles=new Vector3(0,180,0);
         // obj.GetComponent<FlowController>().speed = this.speed;
