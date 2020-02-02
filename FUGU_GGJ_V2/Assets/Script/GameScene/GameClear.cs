46d5886b42d544336d6a8472de9a193ef6f6a0ed
b1017159 <b1017159@fun.ac.jp>
2020-02-02 15:53:07 +0900

    ブラックアウト画面調整

--- FUGU_GGJ_V2/Assets/Script/GameScene/GameClear.cs
+++ FUGU_GGJ_V2/Assets/Script/GameScene/GameClear.cs
@@ -1,21 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameClear : MonoBehaviour
 {
         public GameObject Button;
         public GameObject Maneger;
         GameManager flag;
+        Image image_component;
+        float title_color=0.0f;//タイトルを黒くする
+        float title_clear=0.5f;//タイトルを透明化
         void Start()
         {
                 flag=Maneger.GetComponent<GameManager>();
+                image_component = GetComponent<Image>();
         }
 
         void Update()
         {
-                if(flag.EndedEndingPerform3==true) {
+                //float title_clear=0.5f;
+                if(flag.EndedEndingPerform3==true) {//ゲームクリアするとtrueになる
                         Button.gameObject.SetActive (true);
+                        image_component.color=new Color(title_color,title_color,title_color,title_clear);
                 }
+        }
+        public void Click(){
+
         }
 }
