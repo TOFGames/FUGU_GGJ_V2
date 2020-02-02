9fa95bc04d85c5934a0839617527a24fccf3a115
jyackfrost123 <jyackfrost123@gmail.com>
2020-02-02 11:45:06 +0900

    change difficulty and Canvas

--- FUGU_GGJ_V2/Assets/adjust_diff/TestCanvasController.cs
+++ FUGU_GGJ_V2/Assets/adjust_diff/TestCanvasController.cs
@@ -7,19 +7,37 @@ public class TestCanvasController : MonoBehaviour
 {
 
     public Text scoreText;
-    ParameterManager pra;
+    ParameterManager para;
+    string peaceExpression;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        scoreText = GameObject.Find("scoreTest").GetComponent<Text>();
-        pra = GameObject.Find("ParameterManager").GetComponent<ParameterManager>();
+        scoreText = GameObject.Find("scoreText").GetComponent<Text>();
+        para = GameObject.Find("ParameterManager").GetComponent<ParameterManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "peace and order:"+(pra.Score).ToString();
+        //scoreText.text = "peace and order:"+(pra.Score).ToString();
+
+       // if(pre.Score )
+
+        //int score = para.Score;
+
+        if(para.Score < 30){//Ended World
+          peaceExpression = "Ended World";
+        }else if(30 <= para.Score && para.Score < 50) {//MAD MAX
+          peaceExpression = "MAD MAX";
+        }else if(50 <= para.Score && para.Score < 70){//Danger
+          peaceExpression = "Danger";
+        }else if(70 <= para.Score && para.Score < 85){//Safety
+          peaceExpression = "Safety";
+        }else{//Peaceful
+          peaceExpression = "Peaceful";
+        }
+        scoreText.text = "World State:"+peaceExpression;
     }
 }
