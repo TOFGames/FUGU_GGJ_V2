74a3ec0f9e9fe7d8fda3659d3544b71488770a3a
TOFGAMES <tofgames555@gmail.com>
2020-02-02 12:43:30 +0900

    optionボタンをゲーム時にだけ出すように

--- FUGU_GGJ_V2/Assets/Script/Option.cs
+++ FUGU_GGJ_V2/Assets/Script/Option.cs
@@ -5,16 +5,28 @@ using UnityEngine.EventSystems;
 [DefaultExecutionOrder(-100)]
 public class Option : MonoBehaviour
 {
+        private GameManager gameManager;
         public GameObject Slider;
         public static bool Option_swich=false;
         private Vector2 this_position;
         Vector2 mouseposition;
         void Start(){
+                gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
                 this_position=this.transform.position;
         }
 
         void Update()
         {
+                if(gameManager)
+                {
+                        if(gameManager.StartedGame&&!gameManager.EndedGame)    
+                        {
+                                gameObject.SetActive(true);
+                        } else {
+                                gameObject.SetActive(false);
+            }
+                }
+
                 mouseposition=Input.mousePosition;
                 float distance = Vector2.Distance(mouseposition, this_position);
                 if(EventSystem.current.IsPointerOverGameObject()&&distance<30) {
