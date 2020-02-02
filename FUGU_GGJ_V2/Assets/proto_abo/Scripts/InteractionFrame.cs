4bb83564c31aafb1fabb6af7e58b31effb4d4221
TOFGAMES <tofgames555@gmail.com>
2020-02-02 15:59:45 +0900

    ゲームメイン→タイトル

---
+++ FUGU_GGJ_V2/Assets/proto_abo/Scripts/InteractionFrame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class InteractionFrame : MonoBehaviour {
+    //=============================================================
+    private GameManager gameManager;
+    private GameObject _child;
+
+    //=============================================================
+    private void Init () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        _child = transform.Find("Child").gameObject;
+    }
+
+    private void Awake () {
+        Init();
+    }
+
+    private void Start () {
+
+    }
+
+    private void Update () {
+        if(gameManager.EndedEndingPerform2) {
+            _child.SetActive(true);
+        } else {
+            _child.SetActive(false);
+        }
+    }
+}
