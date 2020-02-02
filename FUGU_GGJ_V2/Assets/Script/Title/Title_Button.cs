c3b72b23f6d6b256920f7545a19315cbe16681ad
b1017159 <b1017159@fun.ac.jp>
2020-02-02 13:27:41 +0900

    タイトルにボタンを導入

---
+++ FUGU_GGJ_V2/Assets/Script/Title/Title_Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title_Button : MonoBehaviour
+{
+        public bool StartButton=false;
+        public bool HplayButton=false;
+        public bool RankingButton=false;
+        void Start()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+        public void OnClick()
+        { //押されたときの処理
+                if(StartButton==true) {
+                        AboProto.AudioManager.Instance.PlaySE("iyahaaaa");
+                        Title_Image.Blackout=true;
+                }
+                if(HplayButton==true) {
+
+                }
+                if(RankingButton==true) {
+                        naichilab.RankingLoader.Instance.SendScoreAndShowRanking (0);
+                }
+        }
+}
---
+++ FUGU_GGJ_V2/Assets/Script/Title/Title_Image.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Title_Image : MonoBehaviour
+{
+        Image image_component;
+        private float canvas_sizeX;//上下の大きさ取得
+        private float canvas_sizeY;
+        float title_color=0.0f;//タイトルを黒くする
+        float title_clear=0.0f;//タイトルを透明化
+        public static bool Blackout=false;
+        void Start()
+        {
+                image_component = GetComponent<Image>();
+                canvas_sizeX=Screen.width;
+                canvas_sizeY=Screen.height;
+                this.transform.localScale = new Vector3(0, 0, 1);
+        }
+
+        void Update()
+        {
+                if(title_clear<1&&Blackout==true) {
+                        this.transform.localScale = new Vector3(canvas_sizeX, canvas_sizeY, 1);
+                        title_clear+=0.01f;
+                        Blackout=true;
+                }
+                image_component.color=new Color(title_color,title_color,title_color,title_clear);
+                if(title_clear>1) SceneManagement.SceneSwich=1;
+        }
+}
